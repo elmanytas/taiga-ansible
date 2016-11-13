316a05ba631f5e706d01fda6ac8f635955da9e59
elmanytas <elmanytas@gmail.com>
2016-11-14 00:02:27 +0100

    Developing taiga-front-dist docker image.

--- docker/taiga-front-dist/docker-entrypoint.sh
+++ docker/taiga-front-dist/docker-entrypoint.sh
@@ -4,15 +4,17 @@ set -e
 
 # Add nginx command if needed
 if [ "${1:0:1}" = '-' ]; then
-	set -- taiga-front-dist "$@"
+	  set -- taiga-front-dist "$@"
 fi
 
 if [ "$1" = 'taiga-front-dist' ]; then
-	# Reconfigure taiga-front-dist:
-	# * reconfigure ansible inventory
-	# * launch ansible with config tag+
-	
-	
+    # Reconfigure taiga-front-dist:
+    # * reconfigure ansible inventory
+	  # * launch ansible with config tag:
+	  #   ansible-playbook  /etc/ansible/site.yml --tags=config-${docker_ansible_tag}
+	  if [ "$taiga_back_hostname" ]; then
+			  sed 's/^taiga_back_hostname*/taiga_back_hostname=$taiga_back_hostname/' /etc/ansible/hosts
+		fi
 fi
 
 # As argument is not related to taiga-front-dist,
