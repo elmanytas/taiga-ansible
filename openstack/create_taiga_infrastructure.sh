cca771c4b4bb72dbe8e6fbfae841dbd17d5a9aee
elmanytas <elmanytas@gmail.com>
2016-11-16 23:21:28 +0100

    Disable celery and events.

--- openstack/create_taiga_infrastructure.sh
+++ openstack/create_taiga_infrastructure.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 heat stack-create router --template-file=router-heat-template.yaml
 sleep 15s
-for component in postgresql redis rabbitmq taiga-back taiga-front-dist taiga-events celery; do
+for component in postgresql redis taiga-back taiga-front-dist; do
     echo Creating $component
     heat stack-create $component --template-file=$component-heat-template.yaml
 done
