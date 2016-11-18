c071c113a8bedf53e2187ae4b0023cebd5ecccd1
elmanytas <elmanytas@gmail.com>
2016-11-18 11:10:38 +0100

    Little adjustments.

--- openstack/create_taiga_infrastructure.sh
+++ openstack/create_taiga_infrastructure.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-heat stack-create router --template-file=router-heat-template.yaml
-sleep 15s
-for component in postgresql redis taiga-back taiga-front-dist; do
+for component in router postgresql taiga-back taiga-front-dist; do
     echo Creating $component
     heat stack-create $component --template-file=$component-heat-template.yaml
+    sleep 20s
 done
